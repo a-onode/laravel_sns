7bbcdb4e65c39d749dbafc4d86548f5101bdfb5d
*MwfApGRKjs <ak.onodera1230@gmail.com>
2023-06-04 21:50:02 +0900

    オンライン機能の実装

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -56,5 +57,10 @@ class User extends Authenticatable
     public function follows()
     {
         $this->belongsToMany(self::class, 'followers', 'following_id', 'followed_id');
+    }
+
+    public function isOnline()
+    {
+        return Cache::has('user-is-online-' . $this->id);
     }
 }
