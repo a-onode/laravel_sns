adfecc3f5a5b3fe9a748d32f8a3efd67736a2dbf
*MwfApGRKjs <ak.onodera1230@gmail.com>
2023-06-24 16:53:49 +0900

    ドラッグ＆ドロップ機能の実装

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -82,7 +82,7 @@
 
                                     <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
                                         <label for="background-image" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">ヘッダー画像</label>
-                                        <div class="mt-2 sm:col-span-2 sm:mt-0">
+                                        <div class="mt-2 sm:col-span-2 sm:mt-0" id="dropzone">
                                             <div class="flex max-w-2xl justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                                                 <div class="text-center">
                                                     <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
@@ -90,12 +90,12 @@
                                                             d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
                                                             clip-rule="evenodd" />
                                                     </svg>
-                                                    <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                                                    <div class="mt-4 flex flex-col text-sm leading-6 text-gray-600">
                                                         <label for="background-image" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                                                            <span>ファイルを選択する</span>
+                                                            <span id="selected-file">ファイルを選択する</span>
                                                             <input id="background-image" name="background-image" type="file" class="sr-only">
                                                         </label>
-                                                        <p class="pl-1">もしくはドラック＆ドロップする</p>
+                                                        <p class="pl-1" id="drop-text">もしくはドラック＆ドロップする</p>
                                                     </div>
                                                 </div>
                                             </div>
@@ -104,7 +104,6 @@
                                 </div>
                             </div>
                         </div>
-
                         <div class="mt-6 flex items-center justify-end gap-x-6">
                             <button type="button" class="text-sm font-semibold leading-6 text-gray-900" onclick="location.href='{{ route('users.index') }}'">キャンセル</button>
                             <button type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">保存する</button>
@@ -115,7 +114,8 @@
         </div>
     </div>
     <script>
-        'use strict'
+        'use strict';
+
         const fileForm = document.getElementById('image');
         const fileName = document.getElementById('file-name');
 
@@ -125,6 +125,30 @@
                 console.log(inputFile.name);
                 fileName.innerText = inputFile.name;
             }
+        });
+
+        const dropzone = document.getElementById('dropzone');
+        const dropText = document.getElementById('drop-text');
+        const selectedFile = document.getElementById('selected-file');
+        const fileInput = document.getElementById('background-image');
+
+        dropzone.addEventListener('dragenter', () => {
+            dropText.innerText = 'ここへドロップしてください';
+        });
+
+        dropzone.addEventListener('dragleave', () => {
+            dropText.innerText = 'もしくはドラック＆ドロップする';
+        });
+
+        dropzone.addEventListener('drop', e => {
+            e.preventDefault();
+            fileInput.files = e.dataTransfer.files;
+            let fileName = e.dataTransfer.files[0].name;
+            dropText.innerText = fileName;
+        });
+
+        dropzone.addEventListener('dragover', e => {
+            e.preventDefault();
         });
     </script>
 </x-app-layout>
